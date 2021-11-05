b0704a9a8f35cb29ac626cb718171e41d0338e28
Hyodar <franco.b.gomes@gmail.com>
2021-11-05 14:04:00 -0300

    Remove casts from sum and sub in operator

--- project/operator.vhd
+++ project/operator.vhd
@@ -17,8 +17,8 @@ end entity;
 
 architecture a_operator of operator is
 begin
-    sum <= unsigned(signed(x) + signed(y));
-    sub <= unsigned(signed(x) - signed(y));
+    sum <= x + y;
+    sub <= x - y;
     greater <= to_unsigned(1, reg_size) when signed(x) > signed(y) else to_unsigned(0, reg_size);
     negative_x <= to_unsigned(1, reg_size) when signed(x) < 0 else to_unsigned(0, reg_size);
 end architecture;
